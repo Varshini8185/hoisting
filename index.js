300d8a50464adeaeb9eda045cec6c7d17469d4a6
Varshini8185 <varshinikatru123@gmail.com>
2024-12-23 22:49:01 +0530

    file

---
+++ index.js
@@ -0,0 +1,15 @@
+let students=[
+    {id:1,s_name:"Pavan",city:"Hyd"},
+    {id:2,s_name:"Arjun",city:"Sec"},
+    {id:3,s_name:"Meena",city:"Vizag"},
+    {id:4,s_name:"Rahul",city:"Bengaluru"},
+    {id:5,s_name:"Sita",city:"Chennai"},
+    {id:6,s_name:"Vijay",city:"Mumbai"}
+];
+for (i in students){
+    console.log(`${students[i].s_name} from ${students[i].city}`)
+}
+for(students of students)
+    {
+    console.log(`${students.s_name} from ${students.city}`)
+}
